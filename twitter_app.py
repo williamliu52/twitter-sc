e5bb57089f64f34db7b2a16b4f48b2ad301a0b5d
WILL LIU <wrliu@uwaterloo.ca>
2017-07-12 01:31:43 -0400

    Create requirements file for virturalenv. Got Python file to start sending Tweets

--- twitter_app.py
+++ twitter_app.py
@@ -9,12 +9,12 @@ ACCESS_TOKEN = '393038677-QGiOscLRjyS46BWwBrjG4DYukdNS7m16h3PGSAoz'
 ACCESS_SECRET = 'W8BmcuB8Ezf0IScLJroGZscp97z4NCH4lllHlHR9VBXCt'
 CONSUMER_KEY = '1piLrcfKuRxSi8rWpJ6j9C2pJ'
 CONSUMER_SECRET = 'iMkNO6otUOVySGqOBYkHsXDDz5ewQc3x7nfEupdKZOHDKnbApL'
-my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_TOKEN, ACCESS_SECRET)
+my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
 def get_tweets():
     # Query params according for Twitter's streaming API
     # Reference: https://dev.twitter.com/streaming/reference/post/statuses/filter
-    sports_leagues = nba,mlb,nfl,nhl,mls
+    sports_leagues = 'nba,mlb,nfl,nhl,mls'
     url = 'https://stream.twitter.com/1.1/statuses/filter.json'
 
     query_data = [('language', 'en'),('track', sports_leagues)]
@@ -36,14 +36,21 @@ def send_tweets_to_spark(response, tcp_connection):
             e = sys.exc_info()[0]
             print("Error: %s" % e)
 
+# Connection constants
 TCP_IP = "localhost"
 TCP_PORT = 9009
 conn = None
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Create server socket to serve Tweets for 1 connection
+serverSocket.bind((TCP_IP, TCP_PORT))
+serverSocket.listen(1)
+# Create client socket to connect and stream Tweets
+clientSocket.connect((TCP_IP, TCP_PORT))
+
 print("Waiting for TCP connection...")
-conn, addr = s.accept()
+conn, addr = serverSocket.accept()
 print("Connected... Starting getting tweets.")
 resp = get_tweets()
 send_tweets_to_spark(resp, conn)
