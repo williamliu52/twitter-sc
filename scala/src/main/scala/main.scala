eb1dfe560495e2d70544a457b49567a943b4f250
WILL LIU <wrliu@uwaterloo.ca>
2017-07-30 15:36:12 -0400

    Completed first compilable version of processRDD function. The function runs on each RDD and creates a DataFrame on the hashtag data.

--- scala/src/main/scala/main.scala
+++ scala/src/main/scala/main.scala
@@ -1,11 +1,14 @@
 import org.apache.spark
 import org.apache.spark._
-import org.apache,spark.streaming._
+import org.apache.spark.streaming._
 import org.apache.spark.sql._
 import org.apache.spark.rdd._
+import org.apache.spark.sql.types._
 
 object TwitterSC {
     def main(args: Array[String]) {
+        // Create SparkSession for SQL
+        val sparkSesh = SparkSession.builder().getOrCreate()
         // Create StreamingContext with 2 threads and batch interval of 2 seconds
         val conf = new SparkConf().setMaster("local[2]").setAppName("TwitterSC")
         val sc = new SparkContext(conf)
@@ -25,8 +28,7 @@ object TwitterSC {
         val tagTotals = hashtags.updateStateByKey[Int](updateTagCount _)
         // Process each RDD generated in each interval
         // TODO: implement processRDD function
-        tagTotals.foreachRDD(processRDD _)
-
+        tagTotals.foreachRDD(processRDD(_, _, sparkSesh))
 
         // Run application
         ssc.start()
@@ -38,16 +40,26 @@ object TwitterSC {
         Some(newCount)
     }
 
-    def processRDD(rdd: RDD[(String, Int)], time: Time) {
-        println("-------- " + time + " --------")
+    def processRDD(rdd: RDD[(String, Int)], time: Time, sparkSesh: SparkSession) {
+        println("-----------" + time + " -----------")
         try {
-            val sqlContext = getSqlContext(rdd.context)
+            // convert RDD to row RDD; need _1 because entries are tuples
+            val rowRDD = rdd.map(entry => Row(entry._1, entry._2))
+            // constructing schema to match structure of rows in rowRDD
+            val schemaString = "hashtag count"
+            val fields = schemaString.split(" ").map(field => StructField(field, StringType, nullable=true))
+            val schema = StructType(fields)
+            // create dataFrame using rowRDD and schema
+            val hashtagsDF = sparkSesh.createDataFrame(rowRDD, schema)
+            // create temp view using dataFrame
+            hashtagsDF.createOrReplaceTempView("hashtags")
+            // select top 10 hashtags
+            val results = sparkSesh.sql("SELECT hashtag, count FROM hashtags ORDER BY count DESC LIMIT 10")
+            results.show()
+            // send top hashtags to web application
+            // TODO: implement sendDataToApp function
         } catch {
             case e : Throwable => println("processRDD exception: " + e)
         }
-    }
-
-    def getSqlContext(sc: SparkContext): SQLContext {
-    // TODO: create a global variable or something that houses a bunch of SQLContext objects; probably pass it in as an argument    
     }
 }
