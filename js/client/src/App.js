667efc0a2d786916e252ac10f916b922291fb32d
Will Liu <will.r.liu@gmail.com>
2017-09-19 01:00:20 -0400

    Create React page that gets data from Node server and display videos.
    package.json - Add proxy value so requests can be proxied by Webpack
    index.html - Add Twitter's widget code so Embeds can be properly displayed
    App.js - Created 2 methods, getAllUrls and getEmbed. GetAllUrls gets all the minified video URLs from the server then one by one calls getEmbed on them, which calls to the server to get the actual Embed code. It is then displayed in an embeddedTweets array of divs. Note that for the embed to work the React method 'dangerouslySetInnerHtml' had to be used, or else the code would just be a string.

--- js/client/src/App.js
+++ js/client/src/App.js
@@ -1,21 +1,84 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            embeds: []
+        }
+    }
+
+    componentDidUpdate() {
+        window.twttr.widgets.load()
+    }
+
+    getAllUrls = function() {
+        fetch('/getUrls', {
+            method: 'get'
+        }).then(resp => {
+            if (!resp.ok) {
+                throw new Error(`status ${resp.status}`);
+            }
+            return resp.json();
+        }).then(json => {
+            this.setState({
+                videos: json.videos || []
+            });
+        }).then(() => {
+            for (let url of this.state.videos) {
+                this.getEmbed(url);
+            }
+        });
+    }
+
+    getEmbed = function(url) {
+        let param = {
+            url: url
+        };
+        fetch('/getEmbed', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'post',
+            body: JSON.stringify(param)
+        }).then(resp => {
+            if (!resp.ok) {
+                throw new Error(`status ${resp.status}`);
+            }
+            return resp.json();
+        }).then(json => {
+            return json.html;
+        }).then(html => {
+            if (this.state.embeds.indexOf(html) === -1) {
+                let newEmbeds = this.state.embeds.push(html);
+                this.setState({
+                    embed: newEmbeds
+                });
+            }
+        });
+    }
+
+    render() {
+        const embeddedTweets = this.state.embeds.map((embed, idx) => (
+            <div key={idx} dangerouslySetInnerHTML={{__html: embed}}>
+            </div>
+        ));
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>TwitterSC</h2>
+                </div>
+                <Button bsSize="large" onClick={this.getAllUrls.bind(this)}>Refresh Videos</Button>
+                {embeddedTweets}
+            </div>
+        );
+    }
 }
 
 export default App;
